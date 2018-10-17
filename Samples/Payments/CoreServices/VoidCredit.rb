e1f94ccbfa05d04fdd01a2e9a7fc82293e5fd935
Sugumar <ansuguma@visa.com>
2018-10-17 15:07:33 +0530

    Modified/Added TMS files

--- Samples/Payments/CoreServices/VoidCredit.rb
+++ Samples/Payments/CoreServices/VoidCredit.rb
@@ -4,7 +4,7 @@ public
 class VoidCredit
   def main
     id = "5336378279036983404106"
-    request = CyberSource::VoidCreditPayment.new
+    request = CyberSource::VoidCreditRequest.new
     apiClient = CyberSource::ApiClient.new
     apiInstance = CyberSource::VoidApi.new(apiClient)
 
@@ -12,34 +12,6 @@ class VoidCredit
     clientReferenceInformation.code = "12345678"
     request.client_reference_information = clientReferenceInformation
     
-    orderInformation = CyberSource::V2paymentsOrderInformation.new
-    billtoInformation = CyberSource::V2paymentsOrderInformationBillTo.new
-    billtoInformation.country = "US"
-    billtoInformation.last_name = "test"
-    billtoInformation.address1 = "201 S. Division St."
-    billtoInformation.postal_code = "48104-2201"
-    billtoInformation.locality = "Ann Arbor"
-    billtoInformation.administrative_area = "MI"
-    billtoInformation.first_name = "test"
-    billtoInformation.phone_number = "999999999"
-    billtoInformation.email = "test@cybs.com"
-    orderInformation.bill_to = billtoInformation
-    request.order_information = orderInformation
-
-    amountInformation = CyberSource::V2paymentsOrderInformationAmountDetails.new
-    amountInformation.total_amount = "102.21"
-    amountInformation.currency = "USD"
-    orderInformation.amount_details = amountInformation
-    request.order_information = orderInformation
-
-    paymentInformation = CyberSource::V2paymentsPaymentInformation.new
-    cardInformation =CyberSource::V2paymentsPaymentInformationCard.new
-    cardInformation.expiration_year = "2031"
-    cardInformation.number = "5555555555554444"
-    cardInformation.expiration_month = "12"
-    cardInformation.type = "002"
-    paymentInformation.card = cardInformation
-    request.payment_information = paymentInformation
     data, status_code, headers = apiInstance.void_credit(request, id)
     puts data, status_code, headers
   rescue StandardError => err
--- Samples/TMS/CoreServices/RetrieveInstrumentIdentifier.rb
+++ Samples/TMS/CoreServices/RetrieveInstrumentIdentifier.rb
@@ -1,21 +1,21 @@
 require 'cyberSource_client'
 
 # * This is a sample code to call InstrumentIdentifierApi,
-# * Delete an Instrument Identifier
-# * Include the profileId and instrument Id in the DELETE request to delete a instrument identifier.
+# * Retrieve an Instrument Identifier
+# * Include the profileId and instrument Id in the GET request to retrieve instrument identifier.
 
 public
-class RemoveInstrumentIdentifier
+class RetrieveInstrumentIdentifier
   def main
     id = '7020000000000137654'
     profileId = '93B32398-AD51-4CC2-A682-EA3E93614EB1'
     apiClient = CyberSource::ApiClient.new
     apiInstance = CyberSource::InstrumentIdentifierApi.new(apiClient)
-    data, status_code, headers = apiInstance.instrumentidentifiers_token_id_delete(profileId, id)
+    data, status_code, headers = apiInstance.instrumentidentifiers_token_id_get(profileId, id)
     puts data, status_code, headers
   rescue StandardError => err
     puts err.message
     puts err.backtrace
   end
-  RemoveInstrumentIdentifier.new.main
+  RetrieveInstrumentIdentifier.new.main
 end
